06baa2bba95f311f79cdf8905fff6a6ae735bcc6
Anthony Polito <anthonyalexanderpolito@gmail.com>
2020-07-18 22:31:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  cool_colors = [blue, black, red, pink]
+  puppies = ["bulldog", "terrier", "poodle", "pitbull"]
 end
 
 def add_element_to_end_of_array(array, element)
